1d145ef2c3a4532248803b3202765a16ea987242
Sam Gubernikoff <sgubernikoff@gmail.com>
2022-03-31 17:33:51 -0400

    pornhub update

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 import music from "../music.mp3";
+import september from "../september.mp3"
 import ReactAudioPlayer from "react-audio-player";
+import pornhub from "../pornhub.mp3"
 
-console.log(music);
+console.log(september);
+console.log(pornhub);
 
 function Homepage() {
 
   const songs = [
+    "/static/media/pornhub.a303a3293a3306e99436.mp3",
+    "/static/media/september.56eadbba04f59f05b1bb.mp3",
     "/static/media/music.c0692109f563108e096a.mp3"
   ];
 
